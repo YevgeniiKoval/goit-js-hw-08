b336fa952576468d446e3d3fa50bdb4fe4bab32c
YevgeniiKoval <109030577+YevgeniiKoval@users.noreply.github.com>
2023-02-07 22:01:32 +0200

    HW

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+updateForm();
+
+formEl.addEventListener('input', throttle(onFormInput,500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+   
+};
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const {
+        elements: { email, message }
+      } = e.target;
+      
+    
+      if (email.value === "" || message.value === "") {
+        return
+      };
+    ({'Email': email.value, 'Message': message.value});
+    formEl.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    
+};
+
+
+function updateForm() {
+    if (localStorage.getItem(STORAGE_KEY) === null) {
+return
+    }
+    const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+      Object.entries(savedForm).forEach(([name,value]) => {
+        formData[name] = value;
+        formEl.elements[name].value = value;
+      });
+};
+
 
